9e33459f0434797b961e92e76f775f6d634f1c5b
Yougeshwar <ypkkhatri@gmail.com>
2016-04-02 00:55:32 +0500

    SB-218 Implement log tailing
    - Resource added
    - AngularJS controller created, ajax call ready
    
    - respone process remaining

--- logback-configuration-rest/src/test/webapp/static/js/tailfileapp.js
+++ logback-configuration-rest/src/test/webapp/static/js/tailfileapp.js
@@ -1,5 +1,23 @@
 var tailFilesApp = angular.module("tailFilesApp",[]);
 
-tailFilesApp.controller("TailFilesController", function ($scope) {
-    //$scope
+tailFilesApp.controller("TailFilesController", function ($scope, $http) {
+    $scope.filename = "log.log";
+    $scope.newlog = "";
+
+    // http://localhost:48080/logging/partial-log/test.log?offset=0
+
+    $scope.contextBaseUrl = "http://localhost:48080";
+    $scope.logfile = "test.log";
+    $scope.offset = 0;
+
+    //$scope.url = "/logging/partial-log/" + $scope.logfile + "?offset=" + offset;
+
+    setInterval(function() {
+        var url = $scope.contextBaseUrl + "/logging/partial-log/" + $scope.logfile + "?offset=" + $scope.offset;
+        $http.get(url).then(function(response) {
+            var data = response.data;
+            alert(data);
+        });
+    }, 10000);
+
 });
